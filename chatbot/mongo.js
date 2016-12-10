824d503b94210b8e9bd0bce422ccc2011604a301
Elldorin <anton.mtodorov@gmail.com>
2016-12-10 08:08:37 +0000

    Mongodb skeleton

---
+++ chatbot/mongo.js
@@ -0,0 +1,19 @@
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/mongo';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    var collection = db.collection('users');
+
+    collection.insert({"data" : "gdfsgdf"});
+  }
+});
