8f997d009916c9c95a6da9e5de5874fa982f12d8
ivalkov <iv.v.valkov@gmail.com>
2016-12-10 07:27:38 +0000

    Fixed final question

--- chatbot/scripted.js
+++ chatbot/scripted.js
@@ -16,7 +16,7 @@ bot.trainAll([
     new TrainingDocument('Hello','are you there?'), 
     new TrainingDocument('Hello','mum?'), 
     new TrainingDocument('Hello', "It's me, Ivan!"),
-    new TrainingDocument('Hello'), "Mum,stay where u are"),
+    new TrainingDocument('Hello', "Mum,stay where u are"),
 
     new TrainingDocument('Mum_how_was_your_day', 'Mum, how was your day?'),
     new TrainingDocument('How_much_did_you_do', "Did you work a lot? "),
@@ -24,9 +24,9 @@ bot.trainAll([
     new TrainingDocument('She_is_fine', "Did you see the Dr?"),
     new TrainingDocument('Yes_we_did', "You did, Ill call u!"),
     new TrainingDocument('On_the_phone', "Mum, ur on the phone"),
-    new TrainingDocument('Worrying_me'), "Have you eaten today?"),
-    new TrainingDocument('Is_confused'), "You are worrying me!"),
-    new TrainingDocument('Where_are_you'), "Where are you?"),
+    new TrainingDocument('Worrying_me', "Have you eaten today?"),
+    new TrainingDocument('Is_confused', "You are worrying me!"),
+    new TrainingDocument('Where_are_you', "Where are you?"),
 
     new TrainingDocument('Mum_I_cant_find_you', "Mum I can't find you anywhere Where are you?"),
 ], function () {
@@ -82,9 +82,7 @@ const hello = new Skill('hello_skill', 'Hello', function (context, request, resp
             context.repeat += 1;
             return response.send(messageFactory("...",["Hello?", "mum?", "are you there?"]));
         }
-        else return response.send(messageFactory("Memory loss is just one of the symptoms victims of dementia can experience,\ 
-         and it can happen to anyone. Dimentia is a disease, which can one day be eliminated. To find out more or to donate to\
-         our cause, follow the links below.",["Donate", "Learn more", "Start again?"]));     
+        else return response.send(messageFactory("Memory loss is just one of the symptoms victims of dementia can experience, and it can happen to anyone. Dimentia is a disease, which can one day be eliminated. To find out more or to donate to our cause, follow the links below.",["Donate", "Learn more", "Start again?"]));     
 
     }
     else context.repeat = 0;
@@ -105,7 +103,6 @@ bot.addSkill(She_is_fine);
 bot.addSkill(Yes_we_did);
 bot.addSkill(On_the_phone);
 
-bot.addSkill(Mum_I_cant_find_you);
 bot.addSkill(hello);
 
 module.exports = bot;
